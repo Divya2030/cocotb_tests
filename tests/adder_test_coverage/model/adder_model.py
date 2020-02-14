9631945454851ad3fa6ed3b56196b7df061e18ea
Divyanirankari <51925745+Divyanirankari@users.noreply.github.com>
2020-02-14 14:26:05 +0530

    Update adder_model.py

--- tests/adder_test_coverage/model/adder_model.py
+++ tests/adder_test_coverage/model/adder_model.py
@@ -1,17 +1,16 @@
-from cocotb_coverage.coverage import * 
-import logging as log
+from cocotb_coverage.coverage import *
 
-adder_Coverage = coverage_section (
-         CoverPoint("adder_model.a", xf = lambda a,b:a , bins =[5]),
-         CoverPoint("adder_model.b", xf = lambda  a,b:b , bins =[10] ),
+
+
+ADDER_Coverage = coverage_section (
+         CoverPoint("adder_model.a", xf = lambda a,b:a , bins = list(range(0,15))),
+         CoverPoint("adder_model.b", xf = lambda a,b:b ,bins = list(range(0,15)))
      )
 
-
-@adder_Coverage
+@ADDER_Coverage
 def adder_model(a, b):
     """ model of adder """
     return a + b
-
 
 
 
